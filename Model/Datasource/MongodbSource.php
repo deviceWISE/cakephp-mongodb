519868fb2c520d2e6cad2fa25c7e1b1f2fc0d4c2
Jon Bradley <jonathan.bradley@telit.com>
2015-11-24 11:47:59 -0500

    fixing compabaility issues with cake 2.7.7

--- Model/Datasource/MongodbSource.php
+++ Model/Datasource/MongodbSource.php
@@ -361,9 +361,9 @@ class MongodbSource extends DboSource {
     if ($this->connected === false) {
       return false;
     }
-        
+
         $table = $this->fullTableName($Model);
-        
+
     $collection = $this->_db
       ->selectCollection($table);
     return $collection;
@@ -445,7 +445,7 @@ class MongodbSource extends DboSource {
 
     $schema = array();
         $table = $this->fullTableName($Model);
-        
+
     if (!empty($Model->mongoSchema) && is_array($Model->mongoSchema)) {
       $schema = $Model->mongoSchema;
       return $schema + array($Model->primaryKey => $this->_defaultSchema['_id']);
@@ -630,9 +630,9 @@ class MongodbSource extends DboSource {
     if (array_key_exists('conditions', $params)) {
       $params = $params['conditions'];
     }
-        
+
         $table = $this->fullTableName($Model);
-        
+
     try{
       $return = $this->_db
         ->selectCollection($table)
@@ -682,7 +682,7 @@ class MongodbSource extends DboSource {
     $reduce = (empty($params['reduce'])) ? array() : $params['reduce'];
     $options = (empty($params['options'])) ? array() : $params['options'];
         $table = $this->fullTableName($Model);
-        
+
     try{
       $return = $this->_db
         ->selectCollection($table)
@@ -715,7 +715,7 @@ class MongodbSource extends DboSource {
 
     $this->_prepareLogQuery($Model); // just sets a timer
         $table = $this->fullTableName($Model);
-        
+
     try{
       $return = $this->_db
         ->selectCollection($table)
@@ -775,7 +775,7 @@ class MongodbSource extends DboSource {
 
     $this->_convertId($data['_id']);
         $table = $this->fullTableName($Model);
-        
+
     try{
       $mongoCollectionObj = $this->_db
         ->selectCollection($table);
@@ -993,9 +993,9 @@ class MongodbSource extends DboSource {
       $id = $conditions['id'];
       unset($conditions['id']);
     }
-        
+
         $table = $this->fullTableName($Model);
-        
+
     $mongoCollectionObj = $this->_db
       ->selectCollection($table);
 
@@ -1393,7 +1393,7 @@ class MongodbSource extends DboSource {
  * @return mixed Prepared value or array of values.
  * @access public
  */
-  public function value($data, $column = null) {
+  public function value($data, $column = null, $null = true) {
     if (is_array($data) && !empty($data)) {
       return array_map(
         array(&$this, 'value'),
